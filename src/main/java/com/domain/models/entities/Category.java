4078db4a7a079ec4ab900851e8d3d77827274370
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-16 12:48:31 +0700

    Relasi entity JPA SPring Boot

--- src/main/java/com/domain/models/entities/Category.java
+++ src/main/java/com/domain/models/entities/Category.java
@@ -1,5 +1,7 @@
 package com.domain.models.entities;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,7 +11,9 @@ import jakarta.persistence.Table;
 
 @Entity 
 @Table(name="tbl_categories")
-public class Category {
+public class Category implements Serializable{
+
+    private static final long serialVersionUID = 1L;
 
     @Id // agar jadi primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto inchirement
